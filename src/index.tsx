2f0fb3a5cc4a42bea33ee03d11814fa95b5491b0
Toni Kujanpää <toni.kujanpaa@hotmail.com>
2025-09-24 00:03:14 +0300

    Maintenance mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,28 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./css/index.css";
-import reportWebVitals from "./reportWebVitals";
-import { Routes, Route, HashRouter } from "react-router-dom";
-import App from "./pages/App";
-import Contact from "./pages/Contact";
-import NoPage from "./pages/NoPage";
-import About from "./pages/About";
-import Prices from "./pages/Prices";
-import Terms from "./pages/Terms";
-import Faq from "./pages/Faq";
-import ScrollToTop from "./utils";
 
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
-root.render(
-  <React.StrictMode>
-    <HashRouter>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="about" element={<About />} />
-        <Route path="prices" element={<Prices />} />
-        <Route path="terms" element={<Terms />} />
-        <Route path="faq" element={<Faq />} />
-        <Route path="*" element={<NoPage />} />
-      </Routes>
-    </HashRouter>
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <div
+    style={{
+      height: "100dvh",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "1.5rem",
+      textAlign: "center",
+      background: "#fff",
+      color: "#222",
+      padding: "2rem"
+    }}
+  >
+    Uusitamme nettisivuja.
+    <br />
+    Otathan toistaiseksi yhteyttä suoraan Instagramin kautta
+    <b>@saratuononen_photography</b>
+  </div>
+);
